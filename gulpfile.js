e1ca1f3dd2f5b0fab0fde9514c8aaaeb5d839741
zwt <782093540@qq.com>
2021-01-25 10:14:00 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,26 @@
 // 获取 gulp
-const gulp = require('gulp')
+const {src, dest, watch} = require('gulp')
 // 获取 gulp-stylus 模块
 const rename = require('gulp-rename')
 const stylus = require('gulp-stylus')
 
 // 编译gulp-stylus
 const sass_dir = 'pages/**/*.styl'
-gulp.task('stylus-go', function() {
-    return gulp.src(sass_dir)
+
+function go() {
+    return src(sass_dir)
         .pipe(stylus())
         .on('error', function (error) {
-
             console.error(error.toString())
             this.emit('end')
-
         })
         .pipe(rename({
             extname: ".wxss"
         }))
+        .pipe(dest('pages'))
+}
 
-        .pipe(gulp.dest('pages'))
-});
+exports.go = go
 
-// 在命令行使用 gulp auto 启动此任务
-gulp.task('stylus-run', function () {
-    // 监听文件修改，当文件被修改则执行 images 任务
-    gulp.watch(sass_dir, ['stylus-go'])
-});
+// 在命令行使用 gulp go 启动此任务
+watch(sass_dir, go)
